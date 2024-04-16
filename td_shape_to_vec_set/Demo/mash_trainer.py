9bd180025b07c3593f18399c3769ff6965185bf0
chLi <565353780@qq.com>
2024-04-16 07:48:09 -0500

    update pretrained model loading

--- td_shape_to_vec_set/Demo/mash_trainer.py
+++ td_shape_to_vec_set/Demo/mash_trainer.py
@@ -1,7 +1,7 @@
 def demo():
     from td_shape_to_vec_set.Module.mash_trainer import MashTrainer
 
-    model_file_path = "./output/20240414_12:01:15_lr0.0001_b128_warmup4_train100000_mash40_sh2d4_sh3d4_nheads8_dheah64_depth24/model_last.pth"
+    model_file_path = "./output/20240416_07:27:42_lr0.0001_b128_warmup0_train100000_mash40_sh2d4_sh3d4_nheads8_dheah64_depth24/model_last.pth"
     print_progress = True
 
     mash_trainer = MashTrainer()
