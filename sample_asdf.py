067f41a2e0381747ea5a15a3c5030c47f3532676
chLi <565353780@qq.com>
2024-04-23 20:15:54 +0800

    remove asdf related code

--- sample_asdf.py
+++
@@ -1,4 +0,0 @@
-from td_shape_to_vec_set.Demo.asdf_sampler import demo as demo_sample_asdf
-
-if __name__ == "__main__":
-    demo_sample_asdf()
--- train_asdf.py
+++
@@ -1,4 +0,0 @@
-from td_shape_to_vec_set.Demo.asdf_trainer import demo as demo_train_asdf
-
-if __name__ == "__main__":
-    demo_train_asdf()
