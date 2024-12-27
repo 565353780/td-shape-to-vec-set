0592152b979b51b0cf84fc43bb661b9efb52a214
chLi <565353780@qq.com>
2024-12-27 19:09:32 +0800

    upgrade mash sampler

--- td_shape_to_vec_set/Demo/mash_sampler.py
+++ td_shape_to_vec_set/Demo/mash_sampler.py
@@ -3,56 +3,28 @@ sys.path.append("../ma-sh/")
 sys.path.append('../distribution-manage/')
 
 import os
-import torch
 import open3d as o3d
 from tqdm import tqdm
 from math import sqrt, ceil
 
-from ma_sh.Method.data import toNumpy
-from ma_sh.Method.pcd import getPointCloud
-from ma_sh.Method.render import renderGeometries
-
+from td_shape_to_vec_set.Method.time import getCurrentTime
 from td_shape_to_vec_set.Module.mash_sampler import MashSampler
 
 
 def demo():
-    output_folder_path = "../Results/output-td/"
-    output_folder_path = './output/'
-    model_folder_name_list = os.listdir(output_folder_path)
-
-    valid_model_folder_name_list = []
-    valid_model_folder_name_list.append('2023')
-    for model_folder_name in model_folder_name_list:
-        if "2024" not in model_folder_name:
-            continue
-        if not os.path.isdir(output_folder_path + model_folder_name + "/"):
-            continue
-
-        valid_model_folder_name_list.append(model_folder_name)
-
-    valid_model_folder_name_list.sort()
-    model_folder_path = valid_model_folder_name_list[-1]
-    # model_folder_path = 'pretrain-v5'
-    model_file_path = output_folder_path + model_folder_path + "/model_last.pth"
+    model_file_path = "../../output/shapenet_03001627_v1/total_model_last.pth".replace('../.', '')
+    use_ema = True
     device = "cpu"
 
-    sample_num = 9
-    diffuse_steps = 36
-    category_id = 18
+    sample_num = 1
+    condition = 18
+    diffuse_steps = 18
 
     print(model_file_path)
-    mash_sampler = MashSampler(model_file_path, device)
+    mash_sampler = MashSampler(model_file_path, use_ema, device)
 
     print("start diffuse", sample_num, "mashs....")
-    sampled_array = mash_sampler.sample(sample_num, diffuse_steps, category_id)
-
-    print(
-        sampled_array.shape,
-        sampled_array.max(),
-        sampled_array.min(),
-        sampled_array.mean(),
-        sampled_array.std(),
-    )
+    sampled_array = mash_sampler.sample(sample_num, condition, diffuse_steps)
 
     object_dist = [2, 0, 2]
 
@@ -62,36 +34,31 @@ def demo():
 
     mash_model = mash_sampler.toInitialMashModel()
 
+    timestamp = getCurrentTime()
+    save_folder_path = './output/' + timestamp + '/'
+    os.makedirs(save_folder_path + '/pcd/', exist_ok=True)
+
     for i in tqdm(range(sample_num)):
         mash_params = sampled_array[i]
 
-        if True:
-            sh2d = 2 * mash_sampler.sh_2d_degree + 1
+        sh2d = 2 * mash_sampler.mask_degree + 1
+        ortho_poses = mash_params[:, :6]
+        positions = mash_params[:, 6:9]
+        mask_params = mash_params[:, 9 : 9 + sh2d]
+        sh_params = mash_params[:, 9 + sh2d :]
 
-            rotation_vectors = mash_params[:, :3]
-            positions = mash_params[:, 3:6]
-            mask_params = mash_params[:, 6 : 6 + sh2d]
-            sh_params = mash_params[:, 6 + sh2d :]
-        else:
-            start_idx = 0
-            end_idx = 2 * mash_sampler.sh_2d_degree + 1
-            mask_params = mash_params[:, start_idx:end_idx]
-            sh2d = 2 * mash_sampler.sh_2d_degree + 1
+        mash_model.loadParams(
+            mask_params=mask_params,
+            sh_params=sh_params,
+            positions=positions,
+            ortho6d_poses=ortho_poses
+        )
 
-            start_idx = end_idx
-            end_idx += (mash_sampler.sh_3d_degree + 1) ** 2
-            sh_params = mash_params[:, start_idx:end_idx]
-            start_idx = end_idx
-            end_idx += 3
-            rotation_vectors = mash_params[:, start_idx:end_idx]
-            start_idx = end_idx
-            end_idx += 3
-            positions = mash_params[:, start_idx:end_idx]
+        mash_model.saveParamsFile(save_folder_path + 'mash/sample_' + str(i) + '.npy')
 
-        mash_model.loadParams(mask_params, sh_params, rotation_vectors, positions)
-        mash_pcd = getPointCloud(toNumpy(torch.vstack(mash_model.toSamplePoints()[:2])))
+        mash_pcd = mash_model.toSamplePcd()
 
-        if True:
+        if False:
             translate = [
                 int(i / row_num) * object_dist[0],
                 0 * object_dist[1],
@@ -102,15 +69,10 @@ def demo():
 
         mash_pcd_list.append(mash_pcd)
 
-    if False:
-        renderGeometries(mash_pcd_list, "sample mash point cloud")
-
-    if True:
-        os.makedirs('./output/', exist_ok=True)
-        for i in range(len(mash_pcd_list)):
-            o3d.io.write_point_cloud(
-                "./output/sample_mash_pcd_" + str(i) + ".ply",
-                mash_pcd_list[i],
-                write_ascii=True,
-            )
+    for i in range(len(mash_pcd_list)):
+        o3d.io.write_point_cloud(
+            save_folder_path + "pcd/sample_pcd_" + str(i) + ".ply",
+            mash_pcd_list[i],
+            write_ascii=True,
+        )
     return True
--- td_shape_to_vec_set/Module/mash_sampler.py
+++ td_shape_to_vec_set/Module/mash_sampler.py
@@ -1,39 +1,45 @@
 import os
 import torch
+import numpy as np
 from math import sqrt, ceil
 from tqdm import tqdm
 from typing import Union
 
 from ma_sh.Model.mash import Mash
-from ma_sh.Method.data import toNumpy
-from ma_sh.Method.pcd import getPointCloud
 from ma_sh.Module.o3d_viewer import O3DViewer
+from ma_sh.Module.local_editor import LocalEditor
 
 from td_shape_to_vec_set.Model.edm_pre_cond import EDMPrecond
 
 
 class MashSampler(object):
     def __init__(
-        self, model_file_path: Union[str, None] = None, device: str = "cpu"
+        self,
+        model_file_path: Union[str, None] = None,
+        use_ema: bool = True,
+        device: str = "cpu"
     ) -> None:
         self.mash_channel = 400
-        self.sh_2d_degree = 3
-        self.sh_3d_degree = 2
-        self.channels = int(
-            6 + (2 * self.sh_2d_degree + 1) + ((self.sh_3d_degree + 1) ** 2)
-        )
-        self.n_heads = 1
+        self.mask_degree = 3
+        self.sh_degree = 2
+        self.context_dim = 512
+        self.n_heads = 8
         self.d_head = 64
-        self.depth = 12
+        self.depth = 24
 
+        self.use_ema = use_ema
         self.device = device
 
+        self.channels = int(
+            9 + (2 * self.mask_degree + 1) + ((self.sh_degree + 1) ** 2)
+        )
         self.model = EDMPrecond(
             n_latents=self.mash_channel,
             channels=self.channels,
             n_heads=self.n_heads,
             d_head=self.d_head,
             depth=self.depth,
+            context_dim=self.context_dim,
         ).to(self.device)
 
         if model_file_path is not None:
@@ -43,46 +49,56 @@ class MashSampler(object):
     def toInitialMashModel(self) -> Mash:
         mash_model = Mash(
             self.mash_channel,
-            self.sh_2d_degree,
-            self.sh_3d_degree,
-            dtype=torch.float32,
+            self.mask_degree,
+            self.sh_degree,
+            20,
+            800,
+            0.4,
+            dtype=torch.float64,
             device=self.device,
         )
         return mash_model
 
-    def loadModel(self, model_file_path, resume_model_only=True):
+    def loadModel(self, model_file_path: str) -> bool:
         if not os.path.exists(model_file_path):
             print("[ERROR][MashSampler::loadModel]")
             print("\t model_file not exist!")
+            print('\t model_file_path:', model_file_path)
             return False
 
         model_dict = torch.load(model_file_path, map_location=torch.device(self.device))
 
-        self.model.load_state_dict(model_dict["model"])
-
-        if not resume_model_only:
-            # self.optimizer.load_state_dict(model_dict["optimizer"])
-            self.step = model_dict["step"]
-            self.eval_step = model_dict["eval_step"]
-            self.loss_min = model_dict["loss_min"]
-            self.eval_loss_min = model_dict["eval_loss_min"]
-            self.log_folder_name = model_dict["log_folder_name"]
+        if self.use_ema:
+            self.model.load_state_dict(model_dict["ema_model"])
+        else:
+            self.model.load_state_dict(model_dict["model"])
 
         print("[INFO][MashSampler::loadModel]")
         print("\t load model success!")
+        print("\t model_file_path:", model_file_path)
         return True
 
     @torch.no_grad()
     def sample(
         self,
         sample_num: int,
-        diffuse_steps: int,
-        category_id: int = 0,
+        condition: Union[int, np.ndarray] = 0,
+        diffuse_steps: int = 18,
     ) -> torch.Tensor:
         self.model.eval()
 
+        if isinstance(condition, int):
+            condition_tensor = torch.ones([sample_num]).long().to(self.device) * condition
+        elif isinstance(condition, np.ndarray):
+            # condition dim: 1x768
+            condition_tensor = torch.from_numpy(condition).type(torch.float32).to(self.device).repeat(sample_num, 1)
+        else:
+            print('[ERROR][Sampler::sample]')
+            print('\t condition type not valid!')
+            return np.ndarray()
+
         sampled_array = self.model.sample(
-            cond=torch.Tensor([category_id] * sample_num).long().to(self.device),
+            cond=condition_tensor,
             batch_seeds=torch.arange(0, sample_num).to(self.device),
             diffuse_steps=diffuse_steps,
         ).float()
@@ -93,10 +109,20 @@ class MashSampler(object):
     def step_sample(
         self,
         sample_num: int,
-        diffuse_steps: int,
-        category_id: int = 0,
+        condition: Union[int, np.ndarray] = 0,
+        diffuse_steps: int = 18,
     ) -> bool:
         self.model.eval()
+
+        if isinstance(condition, int):
+            condition_tensor = torch.ones([sample_num]).long().to(self.device) * condition
+        elif isinstance(condition, np.ndarray):
+            # condition dim: 1x768
+            condition_tensor = torch.from_numpy(condition).type(torch.float32).to(self.device).repeat(sample_num, 1)
+        else:
+            print('[ERROR][Sampler::sample]')
+            print('\t condition type not valid!')
+            return np.ndarray()
 
         object_dist = [2, 0, 2]
 
@@ -104,7 +130,7 @@ class MashSampler(object):
 
         print("start diffuse", sample_num, "mashs....")
         sampled_array = self.model.sample(
-            cond=torch.Tensor([category_id] * sample_num).long().to(self.device),
+            cond=condition_tensor,
             batch_seeds=torch.arange(0, sample_num).to(self.device),
             diffuse_steps=diffuse_steps,
             step_sample=True,
@@ -124,18 +150,21 @@ class MashSampler(object):
             for j in tqdm(range(sample_num)):
                 mash_params = sampled_array[i][j]
 
-                sh2d = 2 * self.sh_2d_degree + 1
+                sh2d = 2 * self.mask_degree + 1
 
-                rotation_vectors = mash_params[:, :3]
-                positions = mash_params[:, 3:6]
-                mask_params = mash_params[:, 6 : 6 + sh2d]
-                sh_params = mash_params[:, 6 + sh2d :]
+                ortho_poses = mash_params[:, :6]
+                positions = mash_params[:, 6:9]
+                mask_params = mash_params[:, 9 : 9 + sh2d]
+                sh_params = mash_params[:, 9 + sh2d :]
 
                 mash_model.loadParams(
-                    mask_params, sh_params, rotation_vectors, positions
+                    mask_params=mask_params,
+                    sh_params=sh_params,
+                    positions=positions,
+                    ortho6d_poses=ortho_poses
                 )
-                mash_points = toNumpy(torch.vstack(mash_model.toSamplePoints()[:2]))
-                pcd = getPointCloud(mash_points)
+
+                pcd = mash_model.toSamplePcd()
 
                 translate = [
                     int(j / row_num) * object_dist[0],
