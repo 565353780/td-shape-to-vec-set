93d61d7bd05c9a94a13469fe705988411054142c
chLi <565353780@qq.com>
2024-12-27 12:08:35 +0800

    add prefetch generator install

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,6 +1,6 @@
 pip install -U torch torchvision torchaudio
 
 pip install -U h5py timm einops tensorboard transformers \
-  scipy trimesh
+  scipy trimesh prefetch_generator
 
 pip install torch-cluster torch-scatter -f https://data.pyg.org/whl/torch-2.5.1+cu124.html
