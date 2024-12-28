c2fae2288921a3c6f5435ef8f5d4e5be232cb603
chLi <565353780@qq.com>
2024-12-28 16:25:59 +0800

    finish auto sample all iterations

--- td_shape_to_vec_set/Demo/mash_sampler.py
+++ td_shape_to_vec_set/Demo/mash_sampler.py
@@ -1,9 +1,10 @@
 import sys
+
+from td_shape_to_vec_set.Config.shapenet import CATEGORY_IDS
 sys.path.append("../ma-sh/")
 sys.path.append('../distribution-manage/')
 
 import os
-import open3d as o3d
 from tqdm import tqdm
 from math import sqrt, ceil
 
@@ -16,15 +17,16 @@ def demo():
     transformer_id = 'ShapeNet_03001627'
     use_ema = True
     device = "cpu"
-
     sample_num = 9
-    condition = 18
+    condition_name = '03001627'
     diffuse_steps = 18
+    save_results_only = False
 
-    timestamp = getCurrentTime()
-    save_folder_path = './output/sample/' + timestamp + '/'
-    os.makedirs(save_folder_path + '/pcd/', exist_ok=True)
+    condition = CATEGORY_IDS[condition_name]
+    condition_info = 'category/' + condition_name
 
+    time_stamp = getCurrentTime()
+    save_folder_path = None
 
     print(model_file_path)
     mash_sampler = MashSampler(model_file_path, use_ema, device, transformer_id)
@@ -35,53 +37,54 @@ def demo():
     mash_file_path_list = [
         '../ma-sh/output/combined_mash.npy',
     ]
-    sampled_array = mash_sampler.sampleWithFixedAnchors(mash_file_path_list, sample_num, condition, diffuse_steps)[-1]
+    sampled_array = mash_sampler.sampleWithFixedAnchors(mash_file_path_list, sample_num, condition, diffuse_steps)
 
-    object_dist = [2, 0, 2]
+    object_dist = [0, 0, 0]
 
     row_num = ceil(sqrt(sample_num))
 
-    mash_pcd_list = []
-
     mash_model = mash_sampler.toInitialMashModel()
 
-    for i in tqdm(range(sample_num)):
-        mash_params = sampled_array[i]
+    for j in range(len(sampled_array)):
+        if save_results_only:
+            if j != len(sampled_array) - 1:
+                continue
 
-        mash_params = mash_sampler.transformer.inverse_transform(mash_params)
+        if save_folder_path is None:
+            save_folder_path = './output/sample/' + time_stamp + '/'
+        current_save_folder_path = save_folder_path + 'iter_' + str(j) + '/' + condition_info + '/'
 
-        sh2d = 2 * mash_sampler.mask_degree + 1
-        ortho_poses = mash_params[:, :6]
-        positions = mash_params[:, 6:9]
-        mask_params = mash_params[:, 9 : 9 + sh2d]
-        sh_params = mash_params[:, 9 + sh2d :]
+        os.makedirs(current_save_folder_path, exist_ok=True)
 
-        mash_model.loadParams(
-            mask_params=mask_params,
-            sh_params=sh_params,
-            positions=positions,
-            ortho6d_poses=ortho_poses
-        )
+        print("start create mash files,", j + 1, '/', len(sampled_array), "...")
+        for i in tqdm(range(sample_num)):
 
-        mash_model.saveParamsFile(save_folder_path + 'mash/sample_' + str(i) + '.npy')
+            mash_params = sampled_array[j][i]
 
-        mash_pcd = mash_model.toSamplePcd()
+            mash_params = mash_sampler.transformer.inverse_transform(mash_params)
 
-        if False:
+            sh2d = 2 * mash_sampler.mask_degree + 1
+            ortho_poses = mash_params[:, :6]
+            positions = mash_params[:, 6:9]
+            mask_params = mash_params[:, 9 : 9 + sh2d]
+            sh_params = mash_params[:, 9 + sh2d :]
+
+            mash_model.loadParams(
+                mask_params=mask_params,
+                sh_params=sh_params,
+                positions=positions,
+                ortho6d_poses=ortho_poses
+            )
+
             translate = [
                 int(i / row_num) * object_dist[0],
                 0 * object_dist[1],
                 (i % row_num) * object_dist[2],
             ]
 
-            mash_pcd.translate(translate)
+            mash_model.translate(translate)
 
-        mash_pcd_list.append(mash_pcd)
+            mash_model.saveParamsFile(current_save_folder_path + 'mash/sample_' + str(i+1) + '_mash.npy', True)
+            mash_model.saveAsPcdFile(current_save_folder_path + 'pcd/sample_' + str(i+1) + '_pcd.ply', True)
 
-    for i in range(len(mash_pcd_list)):
-        o3d.io.write_point_cloud(
-            save_folder_path + "pcd/sample_pcd_" + str(i) + ".ply",
-            mash_pcd_list[i],
-            write_ascii=True,
-        )
     return True
