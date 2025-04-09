dcf338d1898cff50ddb58ef2e0027ff02f224ca9
chLi <565353780@qq.com>
2025-04-10 01:41:44 +0800

    fix setup bug

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ git clone https://github.com/565353780/base-trainer.git
 cd base-trainer
 ./setup.sh
 
-pip install -U h5py timm einops transformers scipy trimesh
+pip install -U h5py timm einops transformers scipy trimesh pymcubes
 
 pip install torch-cluster torch-scatter -f https://data.pyg.org/whl/torch-2.5.1+cu124.html
