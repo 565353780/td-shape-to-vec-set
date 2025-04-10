67098f71829b83305552ef02978f070384d7411a
chLi <565353780@qq.com>
2025-04-10 14:42:37 +0800

    now support gen all shapes at single forward

--- sample_class_cond.py
+++ sample_class_cond.py
@@ -14,11 +14,13 @@ if __name__ == "__main__":
     ae_pth  = '/home/chli/chLi/Model/3DShape2VecSet/ae/kl_d512_m512_l8/checkpoint-199.pth'
     dm_pth  = '/home/chli/chLi/Model/3DShape2VecSet/class_cond_dm/kl_d512_m512_l8_d24_edm/checkpoint-499.pth'
 
+    total = 20
+
     ae_device = torch.device('cpu')
     model_device = torch.device('cuda:0')
 
     dm = 'kl_d512_m512_l8_d24_edm'
-    save_result_folder_path = './output/gen_class_cond/' + dm + '/'
+    save_result_folder_path = '/home/chli/chLi/Results/3DShape2VecSet/output/gen_class_cond/' + dm + '/'
     os.makedirs(save_result_folder_path, exist_ok=True)
 
     ae = kl_d512_m512_l8()
@@ -39,9 +41,6 @@ if __name__ == "__main__":
     z = np.linspace(-1, 1, density+1)
     xv, yv, zv = np.meshgrid(x, y, z)
     grid = torch.from_numpy(np.stack([xv, yv, zv]).astype(np.float32)).view(3, -1).transpose(0, 1)[None].to(ae_device, non_blocking=True)
-
-    total = 10
-    iters = 4
 
     valid_category_id_list = [
         '02691156', # 0: airplane
@@ -73,32 +72,30 @@ if __name__ == "__main__":
 
     with torch.no_grad():
         for category_id in valid_category_id_list:
-            print(category_id)
-            for i in range(total//iters):
-                full_sample_array_list = model.sample(cond=torch.Tensor([category_id]*iters).long().to(model_device), batch_seeds=torch.arange(i*iters, (i+1)*iters).to(model_device))
+            full_sample_array_list = model.sample(cond=torch.Tensor([category_id]*total).long().to(model_device), batch_seeds=torch.arange(0, total).to(model_device))
 
-                for denoise_step, sample_array in enumerate(full_sample_array_list):
-                    sampled_array = sample_array.float().to(ae_device)
+            for denoise_step, sample_array in enumerate(full_sample_array_list):
+                sampled_array = sample_array.float().to(ae_device)
 
-                    print(sampled_array.shape, sampled_array.max(), sampled_array.min(), sampled_array.mean(), sampled_array.std())
+                print(sampled_array.shape, sampled_array.max(), sampled_array.min(), sampled_array.mean(), sampled_array.std())
 
-                    print('start save shapes at denoising step', denoise_step, '...')
-                    for shape_id in trange(sampled_array.shape[0]):
+                print('start save shapes at denoising step', denoise_step, '...')
+                for shape_id in trange(sampled_array.shape[0]):
 
-                        logits = ae.decode(sampled_array[shape_id:shape_id+1], grid)
+                    logits = ae.decode(sampled_array[shape_id:shape_id+1], grid)
 
-                        logits = logits.detach()
+                    logits = logits.detach()
 
-                        volume = logits.view(density+1, density+1, density+1).permute(1, 0, 2).cpu().numpy()
-                        verts, faces = mcubes.marching_cubes(volume, 0)
+                    volume = logits.view(density+1, density+1, density+1).permute(1, 0, 2).cpu().numpy()
+                    verts, faces = mcubes.marching_cubes(volume, 0)
 
-                        verts *= gap
-                        verts -= 1
+                    verts *= gap
+                    verts -= 1
 
-                        m = trimesh.Trimesh(verts, faces)
-                        current_save_shape_path = save_result_folder_path + \
-                            'category_' + str(category_id) + '/' + \
-                            'shape_' + str(shape_id) + '/' + \
-                            'denoise_' + str(denoise_step) + '.obj'
-                        createFileFolder(current_save_shape_path)
-                        m.export(current_save_shape_path)
+                    m = trimesh.Trimesh(verts, faces)
+                    current_save_shape_path = save_result_folder_path + \
+                        'category_' + str(category_id) + '/' + \
+                        'shape_' + str(shape_id) + '/' + \
+                        'denoise_' + str(denoise_step) + '.obj'
+                    createFileFolder(current_save_shape_path)
+                    m.export(current_save_shape_path)
