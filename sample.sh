b841a19a79a5d38d34935e503120a947713e622c
chLi <565353780@qq.com>
2025-04-10 14:45:36 +0800

    remove archive bash file

--- sample.sh
+++
@@ -1,3 +0,0 @@
-python sample_class_cond.py \
-  --ae-pth /home/chli/chLi/Model/3DShape2VecSet/ae/kl_d512_m512_l8/checkpoint-199.pth \
-  --dm-pth /home/chli/chLi/Model/3DShape2VecSet/class_cond_dm/kl_d512_m512_l8_d24_edm/checkpoint-499.pth
