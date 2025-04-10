752a1749236705f743c1949bcd74f340b3b64d7d
chLi <565353780@qq.com>
2025-04-10 14:36:10 +0800

    simplify usage, fix torch.load bug

--- sample_class_cond.py
+++ sample_class_cond.py
@@ -2,37 +2,34 @@ import os
 import torch
 import mcubes
 import trimesh
-import argparse
 import numpy as np
 from tqdm import trange
 
 from td_shape_to_vec_set.Model.models_ae import kl_d512_m512_l8
 from td_shape_to_vec_set.Model.edm_pre_cond import kl_d512_m512_l8_d24_edm
+from td_shape_to_vec_set.Method.path import createFileFolder
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser('', add_help=False)
-    parser.add_argument('--ae-pth', type=str, required=True) # 'output/ae/kl_d512_m512_l16/checkpoint-199.pth'
-    parser.add_argument('--dm-pth', type=str, required=True) # 'output/uncond_dm/kl_d512_m512_l16_edm/checkpoint-999.pth'
-    args = parser.parse_args()
-    print(args)
-
-    dm = 'kl_d512_m512_l8_d24_edm'
-    save_result_folder_path = './output/class_cond_obj/' + dm + '/'
-    os.makedirs(save_result_folder_path, exist_ok=True)
+    ae_pth  = '/home/chli/chLi/Model/3DShape2VecSet/ae/kl_d512_m512_l8/checkpoint-199.pth'
+    dm_pth  = '/home/chli/chLi/Model/3DShape2VecSet/class_cond_dm/kl_d512_m512_l8_d24_edm/checkpoint-499.pth'
 
     ae_device = torch.device('cpu')
     model_device = torch.device('cuda:0')
 
+    dm = 'kl_d512_m512_l8_d24_edm'
+    save_result_folder_path = './output/gen_class_cond/' + dm + '/'
+    os.makedirs(save_result_folder_path, exist_ok=True)
+
     ae = kl_d512_m512_l8()
     ae.eval()
-    ae.load_state_dict(torch.load(args.ae_pth)['model'])
+    ae.load_state_dict(torch.load(ae_pth, weights_only=False)['model'])
     ae.to(ae_device)
 
     model = kl_d512_m512_l8_d24_edm()
     model.eval()
 
-    model.load_state_dict(torch.load(args.dm_pth)['model'])
+    model.load_state_dict(torch.load(dm_pth, weights_only=False)['model'])
     model.to(model_device)
 
     density = 128
@@ -43,7 +40,7 @@ if __name__ == "__main__":
     xv, yv, zv = np.meshgrid(x, y, z)
     grid = torch.from_numpy(np.stack([xv, yv, zv]).astype(np.float32)).view(3, -1).transpose(0, 1)[None].to(ae_device, non_blocking=True)
 
-    total = 4
+    total = 10
     iters = 4
 
     valid_category_id_list = [
@@ -70,27 +67,38 @@ if __name__ == "__main__":
     ]
     valid_category_id_list = [0, 2, 6, 9, 16, 18, 22, 23, 24, 26, 27, 39, 33, 49, 44, 46, 47, 49, 52, 53]
 
+    valid_category_id_list = [
+        18,
+    ]
+
     with torch.no_grad():
         for category_id in valid_category_id_list:
             print(category_id)
             for i in range(total//iters):
-                sampled_array = model.sample(cond=torch.Tensor([category_id]*iters).long().to(model_device), batch_seeds=torch.arange(i*iters, (i+1)*iters).to(model_device))[-1].float().to(ae_device)
+                full_sample_array_list = model.sample(cond=torch.Tensor([category_id]*iters).long().to(model_device), batch_seeds=torch.arange(i*iters, (i+1)*iters).to(model_device))
 
-                print(sampled_array.shape, sampled_array.max(), sampled_array.min(), sampled_array.mean(), sampled_array.std())
+                for denoise_step, sample_array in enumerate(full_sample_array_list):
+                    sampled_array = sample_array.float().to(ae_device)
 
-                for j in trange(sampled_array.shape[0]):
+                    print(sampled_array.shape, sampled_array.max(), sampled_array.min(), sampled_array.mean(), sampled_array.std())
 
-                    logits = ae.decode(sampled_array[j:j+1], grid)
+                    print('start save shapes at denoising step', denoise_step, '...')
+                    for shape_id in trange(sampled_array.shape[0]):
 
-                    logits = logits.detach()
+                        logits = ae.decode(sampled_array[shape_id:shape_id+1], grid)
 
-                    volume = logits.view(density+1, density+1, density+1).permute(1, 0, 2).cpu().numpy()
-                    verts, faces = mcubes.marching_cubes(volume, 0)
+                        logits = logits.detach()
 
-                    verts *= gap
-                    verts -= 1
+                        volume = logits.view(density+1, density+1, density+1).permute(1, 0, 2).cpu().numpy()
+                        verts, faces = mcubes.marching_cubes(volume, 0)
 
-                    m = trimesh.Trimesh(verts, faces)
-                    current_save_result_folder_path = save_result_folder_path + str(category_id) + '/'
-                    os.makedirs(current_save_result_folder_path, exist_ok=True)
-                    m.export(current_save_result_folder_path + '{:05d}.obj'.format(i*iters+j))
+                        verts *= gap
+                        verts -= 1
+
+                        m = trimesh.Trimesh(verts, faces)
+                        current_save_shape_path = save_result_folder_path + \
+                            'category_' + str(category_id) + '/' + \
+                            'shape_' + str(shape_id) + '/' + \
+                            'denoise_' + str(denoise_step) + '.obj'
+                        createFileFolder(current_save_shape_path)
+                        m.export(current_save_shape_path)
